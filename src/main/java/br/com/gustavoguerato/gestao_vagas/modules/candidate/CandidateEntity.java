749a308c26f654437c9c34f9899203a39295fea2
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-07-29 13:52:33 -0300

    validacao de campos

--- src/main/java/br/com/gustavoguerato/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/gustavoguerato/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -13,11 +14,15 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
-    @Pattern(regexp = "^(?!!\\s*$).+", message = "o campo(username) não pode conter espaços")
+
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "o campo(username) não pode conter espaços")
     private String username;
+
     @Email(message = "o campo(email) deve conter um email valido")
     private String email;
-    @Length(min = 10, max = 100)
+
+    @Length(min = 10, max = 100, message = "O campo senha deve ter entre 10 e 100 caracteres")
     private String password;
     private String description;
     private String curriculo;
