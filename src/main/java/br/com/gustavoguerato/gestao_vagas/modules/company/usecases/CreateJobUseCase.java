a73eb63b150fa3d6ba6976f1d8f016cb77718458
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-07-31 19:01:54 -0300

    refatoração JobController

--- src/main/java/br/com/gustavoguerato/gestao_vagas/modules/company/usecases/CreateJobUseCase.java
+++ src/main/java/br/com/gustavoguerato/gestao_vagas/modules/company/usecases/CreateJobUseCase.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.gustavoguerato.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.gustavoguerato.gestao_vagas.modules.company.entities.JobEntity.JobEntityBuilder;
 import br.com.gustavoguerato.gestao_vagas.modules.company.repositories.JobRepository;
 
 @Service
@@ -12,7 +13,7 @@ public class CreateJobUseCase {
     @Autowired
     private JobRepository jobRepository;
 
-    public JobEntity execute(JobEntity jobEntity) {
-        return this.jobRepository.save(jobEntity);
+    public JobEntity execute(JobEntityBuilder jobEntity) {
+        return this.jobRepository.saveAll(jobEntity);
     }
 }
