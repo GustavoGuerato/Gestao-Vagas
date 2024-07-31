cc60888e3af448d44c90105e8e59db5f56725c7d
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-07-31 15:57:39 -0300

    finalizacao da autorizacao

---
+++ src/main/java/br/com/gustavoguerato/gestao_vagas/modules/company/controllers/AuthCompanyController.java
@@ -0,0 +1,33 @@
+package br.com.gustavoguerato.gestao_vagas.modules.company.controllers;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gustavoguerato.gestao_vagas.modules.company.dto.AuthCompanyDto;
+import br.com.gustavoguerato.gestao_vagas.modules.company.usecases.AuthCompanyUseCase;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthCompanyController {
+
+    @Autowired
+    private AuthCompanyUseCase authCompanyUseCase;
+
+    @PostMapping("/company")
+    public ResponseEntity<Object> create(@RequestBody AuthCompanyDto authCompanyDto) {
+        try {
+            var result = this.authCompanyUseCase.execute(authCompanyDto);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+    }
+}
--- src/main/java/br/com/gustavoguerato/gestao_vagas/modules/company/usecases/AuthCompanyUseCase.java
+++ src/main/java/br/com/gustavoguerato/gestao_vagas/modules/company/usecases/AuthCompanyUseCase.java
@@ -3,9 +3,13 @@ package br.com.gustavoguerato.gestao_vagas.modules.company.usecases;
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 import br.com.gustavoguerato.gestao_vagas.modules.company.dto.AuthCompanyDto;
 import br.com.gustavoguerato.gestao_vagas.modules.company.repositories.CompanyRepository;
@@ -13,13 +17,16 @@ import br.com.gustavoguerato.gestao_vagas.modules.company.repositories.CompanyRe
 @Service
 public class AuthCompanyUseCase {
 
+    @Value("${security.token.secret}")
+    private String secretKey;
+
     @Autowired
     private CompanyRepository companyRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void execute(AuthCompanyDto authCompanyDto) throws AuthenticationException {
+    public String execute(AuthCompanyDto authCompanyDto) throws AuthenticationException {
         var company = this.companyRepository.findByUsername(authCompanyDto.getUsername()).orElseThrow(() -> {
             throw new UsernameNotFoundException("Company not found");
 
@@ -29,5 +36,10 @@ public class AuthCompanyUseCase {
         if (!passwordMatches) {
             throw new AuthenticationException();
         }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("Microsoft3")
+                .withSubject(company.getId().toString()).sign(algorithm);
+        return token;
     }
 }
