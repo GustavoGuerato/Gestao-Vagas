0d997d991602d33fb55e99f45aa2d347585412a6
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-08-01 14:32:48 -0300

    criacao do authcandidatecontroller.java

--- src/main/java/br/com/gustavoguerato/gestao_vagas/modules/company/repositories/JobRepository.java
+++ src/main/java/br/com/gustavoguerato/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -5,10 +5,7 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.gustavoguerato.gestao_vagas.modules.company.entities.JobEntity;
-import br.com.gustavoguerato.gestao_vagas.modules.company.entities.JobEntity.JobEntityBuilder;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
-
-    JobEntity saveAll(JobEntityBuilder jobEntity);
 
 }
