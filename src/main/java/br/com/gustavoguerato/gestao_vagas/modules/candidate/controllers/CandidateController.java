bb1ace369af4b58cbb767521c567fab534b2e987
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-08-01 14:59:12 -0300

    criacao do profilecandidateusecase e profilecandidateresponsedto e alguns ajustes

--- src/main/java/br/com/gustavoguerato/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/gustavoguerato/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,15 +2,15 @@ package br.com.gustavoguerato.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.gustavoguerato.gestao_vagas.exceptions.UserFoundException;
 import br.com.gustavoguerato.gestao_vagas.modules.candidate.CandidateEntity;
-import br.com.gustavoguerato.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.gustavoguerato.gestao_vagas.modules.candidate.UseCases.CreateCandidateUseCase;
+import br.com.gustavoguerato.gestao_vagas.modules.company.usecases.ProfileCandidateUseCase;
 import jakarta.validation.Valid;
 
 @RestController
@@ -20,6 +20,9 @@ public class CandidateController {
     @Autowired
     private CreateCandidateUseCase createCandidateUseCase;
 
+    @Autowired
+    private ProfileCandidateUseCase profileCandidateUseCase;
+
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
         try {
@@ -28,6 +31,17 @@ public class CandidateController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Object> get() {
+        try {
+            var profile = this.profileCandidateUseCase.execute(null);
+            return ResponseEntity.ok().body(profile);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
     }
 
 }
---
+++ src/main/java/br/com/gustavoguerato/gestao_vagas/modules/company/usecases/ProfileCandidateUseCase.java
@@ -0,0 +1,27 @@
+package br.com.gustavoguerato.gestao_vagas.modules.company.usecases;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.gustavoguerato.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.gustavoguerato.gestao_vagas.modules.company.dto.ProfileCandidateResponseDTO;
+
+@Service
+public class ProfileCandidateUseCase {
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public ProfileCandidateResponseDTO execute(UUID idCandidate) {
+        var candidate = this.candidateRepository.findById(idCandidate).orElseThrow(() -> {
+            throw new UsernameNotFoundException("User not Found");
+        });
+
+        var candidateDTO = ProfileCandidateResponseDTO.builder().description(candidate.getDescription())
+                .username(candidate.getUsername()).email(candidate.getEmail()).id(candidate.getId())
+                .name(candidate.getName()).build();
+        return candidateDTO;
+    }
+}
